2cba2869fb5a775d6df901468272c4c1969afc5c
lvergara06 <lvergara06@gmail.com>
2023-03-06 09:26:38 -0800

    Firefox executable to open on double click

--- user_to_network_NativeApp/Transport_win.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Options
-:: -s CreateUrl , this option lets the extension send the data to the specified CreateUrl. i.e. -s "https://prototypeapi2022.azurewebsites.net/api/Connection/Create" 
-:: -E All , this option lets the extension send and extended set of data. "All" means all request headers and response headers. i.e. -E All
-:: -o Path, this option lets you pass a list of popup options to the extension for the prompt. i.e. -o "C:/Users/lvergararodr/source/repos/webextensions-examples/Prototype/app/options.txt"
-:: python -u C:\Users\lvergararodr\source\repos\webextensions-examples\Prototype\app\Transport.py -s "https://prototypeapi2022.azurewebsites.net/api/Connection/Create" -E All
-
-python -u C:\Users\lvergararodr\source\repos\lvergara06\user_to_network\user_to_network\user_to_network_NativeApp\Transport.py -o "C:/Users/lvergararodr/source/repos/webextensions-examples/Prototype/app/options.txt"
