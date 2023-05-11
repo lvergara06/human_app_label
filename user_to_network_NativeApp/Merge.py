b240fa65cc2607f4eb7c83cf8a28c576f88055f2
Luis VergarA <lvergara06@gmail.com>
2023-05-10 20:24:31 -0700

    Fixed weird chars and comment line

--- user_to_network_NativeApp/Merge.py
+++ user_to_network_NativeApp/Merge.py
@@ -40,28 +40,28 @@ with open(connectionsFile, 'r') as source1,\
         flowSourcePort = splitFlow[2]
         flowDestinationPort = splitFlow[3]
         flowStartTime = splitFlow[5]
-        // Skip the header fields
+        # Skip the header fields
         if flowStartTime == "TIMESTAMP_START":
             continue
         # This is an adjustment to get mountain to utc timestamp from pmacct
         # If your pmacct timestamps are in utc then you will have to remove this.
         try:
-            # Convert the local time string to a datetime object
-            local_time = datetime.strptime(flowStartTime, "%Y-%m-%d %H:%M:%S.%f")
+        # Convert the local time string to a datetime object
+            local_time = datetime.strptime(flowStartTime, "%Y-%m-%d %H:%M:%S.%f")
 
-            # Get the local timezone
-            local_tz = pytz.timezone('America/Denver')
+        # Get the local timezone
+            local_tz = pytz.timezone('America/Denver')
 
-            # Localize the datetime object to the local timezone
-            localized_time = local_tz.localize(local_time, is_dst=None)
+        # Localize the datetime object to the local timezone
+            localized_time = local_tz.localize(local_time, is_dst=None)
 
-            # Convert the localized datetime to UTC
-            utc_time = localized_time.astimezone(pytz.utc)
+         # Convert the localized datetime to UTC
+            utc_time = localized_time.astimezone(pytz.utc)
 
-            # Calculate the flowEpoch timestamp
-            flowEpoch = int((utc_time - datetime(1970, 1, 1, tzinfo=pytz.utc)).total_seconds() * 1000)
+        # Calculate the flowEpoch timestamp
+            flowEpoch = int((utc_time - datetime(1970, 1, 1, tzinfo=pytz.utc)).total_seconds() * 1000)
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Error: {e}")
         
         #print("flow:" + flowSourceIp + " " + flowDestinationIp + " " + flowSourcePort + " " + flowDestinationPort + " "  + str(flowEpoch) )
         source1.seek(0)
